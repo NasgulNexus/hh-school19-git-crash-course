e7e3c1d0b9372c65f55eaa82a03b653a30550d97
Nikita Bezdolniy <gooverdian@gmail.com>
2019-11-10 23:49:58 +0300

    Add runner script

---
+++ run.py
@@ -0,0 +1,11 @@
+from bubble_sort import bubbleSort
+from heap_sort import heapSort
+
+bubble_sorted = [1,4,2,56,134,7,2]
+bubbleSort(bubble_sorted)
+print(bubble_sorted)
+
+heap_sorted = [25,66,2,7,91,2,3,11,7]
+heapSort(heap_sorted)
+print(heap_sorted)
+
